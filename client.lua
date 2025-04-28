6a23946ab9eed9a888c54f08a476fd05c78d96eb
kubanscripts <130844381+kubanscripts@users.noreply.github.com>
2025-04-28 14:38:30 +1000

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,19 +7,18 @@ local function ToggleScoreboard()
         TriggerServerEvent('kuban_scoreboard:fetchData')
     end
 end
+
 RegisterCommand("scoreboard", function()
     ToggleScoreboard()
 end, false)
+
 RegisterKeyMapping("scoreboard", "Toggle Scoreboard", "keyboard", "HOME")
+
 RegisterNetEvent('kuban_scoreboard:updateData', function(data)
     SendNUIMessage({
         action = "update",
         players = data.players,
-        police = data.police,
-        fire = data.fire,
-        medic = data.medic,
-        mechanic = data.mechanic,
         playerId = data.playerId,
-        jobAndRank = data.jobAndRank
+        jobStatuses = data.jobStatuses
     })
 end)
